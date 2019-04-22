f989d8ebe6523e342899bb6e152dbd56ee9b9b2e
Yuan-Sen Ting <ting.yuansen.astro@gmail.com>
2019-04-21 21:44:40 -0400

    update

--- binspec/process_spectra.py
+++ binspec/process_spectra.py
@@ -1,30 +1,33 @@
 '''
-Code for reading in combined spectra and visit spectra.
-This uses Jo Bovy's APOGEE package to read in spectra: https://github.com/jobovy/apogee
-This code adapted only slightly from that provided by Yuan-Sen Ting. 
+Code for reading in combined and individual visit spectra.
 Any way that you can get your hands on the spectra should be fine, as long as you 
-(a) set the uncertainties high in bad pixels, (b) normalize them using the 
-spectral_model.get_apogee_continuum() function, and (c) set a max S/N of 200
 
-I've only used it for spectra from DR12/13, so it's possible that some  
-changes could be needed for DR14. 
+Here we adopt APOGEE DR14. Edit os.environs below for a later version of APOGEE data release.
+Since our spectral model training set was normalized using the DR12 wavelength definition, 
+even thought the spectra are from DR14, we will resample them into DR12 wavelength format.
 '''
+
 from __future__ import absolute_import, division, print_function # python2 compatibility
 import numpy as np
 import sys
 import os
+import subprocess
+import copy
+import astropy.io.fits as pyfits
 
 from . import utils
 from . import spectral_model
 
-# dr12
-os.environ["SDSS_LOCAL_SAS_MIRROR"] = "data.sdss3.org/sas/"
-os.environ["RESULTS_VERS"] = "v603"
-os.environ["APOGEE_APOKASC_REDUX"] = "v6.2a"
+# dr14
+master_path = "data.sdss3.org/sas/dr14/apogee/spectro/redux/r8/stars/"
+apVisit_path = "data.sdss3.org/sas/dr14/apogee/spectro/redux/r8/apo25m/"
 
-import apogee.tools.read as apread
-from apogee.tools import bitmask
-from apogee.spec import continuum
+catalog_path = "l31c/l31c.2/"
+catalog_name = "allStar-l31c.2.fits"
+all_visit_name = "allVisit-l31c.2.fits"
+
+# download path
+download_path = "apogee_download/"
 
 # read in the default wavelength array and the list of pixels used for fitting
 wavelength = utils.load_wavelength_array()
@@ -34,89 +37,32 @@ def read_apogee_catalog():
     '''
     read in the catalog of info for all stars in a data release. 
     '''
-    all_star_catalog = apread.allStar(rmcommissioning = False, rmdups = False, 
-        main = False, raw = True)
+    filepath = os.path.join(master_path, catalog_path, catalog_name)  # dr14
+    filename = os.path.join(download_path, catalog_name)
+    try:
+        os.makedirs(os.path.dirname(download_path))
+    except OSError: pass
+    if not os.path.exists(filename):
+        print("Downloading : " + catalog_name)
+        subprocess.check_call(["wget", filepath, "-O", "%s"%filename])
+    all_star_catalog = pyfits.getdata(filename)
+    catalog_id = all_star_catalog['APOGEE_ID'].astype("str")
     
-    # and match to allVisit for the fibers that each star was observed in
-    allvdata = apread.allVisit(raw = True)
+    filepath = os.path.join(master_path, catalog_path, all_visit_name)  # dr14
+    filename = os.path.join(download_path, all_visit_name)
+    if not os.path.exists(filename):
+        print("Downloading : " + catalog_name)
+        subprocess.check_call(["wget", filepath, "-O", "%s"%filename])
+    allvdata = pyfits.getdata(filename)
+
     fibers = np.zeros((len(all_star_catalog), 
         np.nanmax(all_star_catalog['NVISITS'])), dtype='int') - 1
-    
     for ii in range(len(all_star_catalog)):
         for jj in range(all_star_catalog['NVISITS'][ii]):
             fibers[ii, jj] = allvdata[all_star_catalog['VISIT_PK'][ii][jj]]['FIBERID']
 
-    return all_star_catalog, fibers
+    return all_star_catalog, catalog_id, fibers
 
-def read_batch_of_spectra(batch_count, batch_size = 10000):
-    '''
-    Download a bunch of *combined* spectra in one go. Set the uncertainties to a large
-    value in bad pixels, normalize, and save the batch locally. 
-    '''
-    # read in the catalog catalog   
-    catalog, fibers = read_apogee_catalog()
-    catalog = catalog[batch_count*batch_size:(batch_count+1)*batch_size]
-    fibers = fibers[batch_count*batch_size:(batch_count+1)*batch_size]
-    _COMBINED_INDEX = 1
-    
-    nspec = len(catalog)
-    spec = np.zeros((nspec, 7214))
-    specerr = np.zeros((nspec, 7214))
-    
-    # Set up bad pixel mask
-    badcombpixmask = bitmask.badpixmask() + 2**bitmask.apogee_pixmask_int("SIG_SKYLINE")
-    
-    # loop through the individual targets
-    for ii in range(nspec):
-        field = catalog['FIELD'][ii]
-        ap_id = catalog['APOGEE_ID'][ii]
-        loc_id = catalog['LOCATION_ID'][ii]
-        print('processing target %d with id %s' % (ii, ap_id))
-        
-        try:
-            if loc_id == 1:
-                temp1 = apread.apStar(field, ap_id, ext = 1, header = False, aspcapWavegrid = True)
-                temp2 = apread.apStar(field, ap_id, ext = 2, header = False, aspcapWavegrid = True)
-                temp3 = apread.apStar(field, ap_id, ext = 3, header = False, aspcapWavegrid = True)
-            else:
-                temp1 = apread.apStar(loc_id, ap_id, ext = 1, header = False, aspcapWavegrid = True)
-                temp2 = apread.apStar(loc_id, ap_id, ext = 2, header = False, aspcapWavegrid = True)
-                temp3 = apread.apStar(loc_id, ap_id, ext = 3, header = False, aspcapWavegrid = True)
-
-            if temp1.shape[0] > 6000:
-                spec[ii] = temp1
-                specerr[ii] = temp2
-                mask = temp3
-            else:
-                spec[ii] = temp1[_COMBINED_INDEX]
-                specerr[ii]= temp2[_COMBINED_INDEX]
-                mask = temp3[_COMBINED_INDEX]
-    
-            # Inflate uncertainties for bad pixels        
-            specerr[ii, (mask & (badcombpixmask)) != 0] += \
-                100. * np.mean(spec[ii, np.isfinite(spec[ii])])
-    
-            # Inflate pixels with high SNR to 0.5
-            highsnr = spec[ii]/specerr[ii] > 200.
-            specerr[ii, highsnr] = 0.005*np.fabs(spec[ii, highsnr])
-
-            # Continuum-normalize
-            cont = utils.get_apogee_continuum(wavelength = wavelength, spec = spec[ii], 
-                spec_err = specerr[ii], cont_pixels = cont_pixels)
-            spec[ii] /= cont
-            specerr[ii] /= cont
-            specerr[ii, highsnr] = 0.005 
-        except OSError:
-            print('target could not be found!')
-            continue
-
-    # save spectra
-    np.savez('spectra/apogee_all_spectra_' + str(batch_count) + '.npz',
-             wavelength = wavelength,
-             spectra = spec,
-             spec_err = specerr,
-             apogee_id = np.array(catalog["APOGEE_ID"]),
-             apogee_fiber_id = fibers)
 
 def get_combined_spectrum_single_object(apogee_id, catalog = None, save_local = False):
     '''
@@ -126,29 +72,36 @@ def get_combined_spectrum_single_object(apogee_id, catalog = None, save_local = 
     '''
     # read in the allStar catalog if you haven't already
     if catalog is None:
-        catalog, fibers = read_apogee_catalog()
-        
-    # Set up bad pixel mask
-    badcombpixmask = bitmask.badpixmask() + 2**bitmask.apogee_pixmask_int("SIG_SKYLINE")
+        catalog, catalog_id, fibers = read_apogee_catalog()
+    
     _COMBINED_INDEX = 1
     
-    msk = np.where(catalog['APOGEE_ID'] == apogee_id)[0]
+    msk = np.where(catalog_id == apogee_id)[0]
     if not len(msk):
         raise ValueError('the desired Apogee ID was not found in the allStar catalog.')
-    
-    field = catalog['FIELD'][msk[0]]
-    ap_id = apogee_id
-    loc_id = catalog['LOCATION_ID'][msk[0]]
-        
-    if loc_id == 1:
-        temp1 = apread.apStar(field, ap_id, ext = 1, header = False, aspcapWavegrid = True)
-        temp2 = apread.apStar(field, ap_id, ext = 2, header = False, aspcapWavegrid = True)
-        temp3 = apread.apStar(field, ap_id, ext = 3, header = False, aspcapWavegrid = True)
-    else:
-        temp1 = apread.apStar(loc_id, ap_id, ext = 1, header = False, aspcapWavegrid = True)
-        temp2 = apread.apStar(loc_id, ap_id, ext = 2, header = False, aspcapWavegrid = True)
-        temp3 = apread.apStar(loc_id, ap_id, ext = 3, header = False, aspcapWavegrid = True)
 
+    field = catalog['FIELD'][msk[0]]
+    loc_id = catalog['LOCATION_ID'][msk[0]]
+
+    filename = 'apStar-r8-%s.fits' % apogee_id.strip()
+    if loc_id == 1:
+        filepath = os.path.join(master_path,'apo1m', field.strip(), filename)
+    else:
+        filepath = os.path.join(master_path,'apo25m', '%i' % loc_id, filename)
+    filename = os.path.join(download_path, filename)
+
+    # download spectrum
+    try:
+        os.makedirs(os.path.dirname(download_path))
+    except OSError: pass
+    if not os.path.exists(filename):
+        subprocess.check_call(["wget", filepath, '-O', '%s'%filename])
+
+    # read spectrum
+    temp1 = pyfits.getdata(filename, ext = 1, header = False)
+    temp2 = pyfits.getdata(filename, ext = 2, header = False)
+    temp3 = pyfits.getdata(filename, ext = 3, header = False)
+    
     if temp1.shape[0] > 6000:
         spec = temp1
         specerr = temp2
@@ -158,24 +111,26 @@ def get_combined_spectrum_single_object(apogee_id, catalog = None, save_local = 
         specerr = temp2[_COMBINED_INDEX]
         mask = temp3[_COMBINED_INDEX]
 
-    # Inflate uncertainties for bad pixels    
-    specerr[(mask & (badcombpixmask)) != 0] += 100*np.mean(spec[np.isfinite(spec)])
-
-    # Inflate pixels with high SNR to 0.5
-    highsnr = spec/specerr > 200.
-    specerr[highsnr] = 0.005*np.fabs(spec[highsnr])
-
-    # Continuum-normalize
+    # convert ApStar grid to Aspcap grid
+    spec = toAspcapGrid(spec) # dr12 wavelength format
+    specerr = toAspcapGrid(specerr)
+    
+    # cull dead pixels
+    choose = spec <= 0
+    spec[choose] = 0.01
+    specerr[choose] = np.max(np.abs(spec))*999.
+        
+    # continuum-normalize
     cont = utils.get_apogee_continuum(wavelength = wavelength, spec = spec, 
         spec_err = specerr, cont_pixels = cont_pixels)
     spec /= cont
     specerr /= cont
-    specerr[highsnr] = 0.005 
     
     if save_local:
-        np.savez('spectra/combined/spectrum_ap_id_' + str(apogee_id.decode()) + '_.npz',
+        np.savez(download_path + 'spectrum_ap_id_' + str(apogee_id) + '_.npz',
                  spectrum = spec, spec_err = specerr)
     return spec, specerr
+
     
 def get_visit_spectra_individual_object(apogee_id, allvisit_cat = None, save_local = False):
     '''
@@ -183,34 +138,18 @@ def get_visit_spectra_individual_object(apogee_id, allvisit_cat = None, save_loc
     Get the v_helios from the allStar catalog, which are more accurate than 
         the values reported in the visit spectra fits files. 
     Use the barycentric correction to shift spectra to the heliocentric frame.
-    Do a preliminary normalization using Bovy's visit spectrum normalization tool. 
+    Do a preliminary normalization similar to Bovy's routine
         It's critical that the spectra be renormalized prior to fitting using the 
         spectral_model.get_apogee_continuum() function for self-consistency. 
-    apogee_id = byte-like object, i.e. b'2M06133561+2433362'
-    
-    ##### NOTE #####
-    I had to make some minor changes to Bovy's APOGEE package to get it to 
-    properly read in the visit spectra. In particular, after the 3rd line of 
-    the function apVisit() in that package's /apogee/tools/read.py file, I added
-    the following lines:
-    
-    if ext == 0:
-        from astropy.io import fits
-        hdulist = fits.open(filePath)
-        header = hdulist[0].header 
-        hdulist.close()
-        return header
-    
-    I think this is the only change that was necessary, but it's possible I'm 
-    forgetting another change. 
+    apogee_id = byte-like object, i.e. '2M06133561+2433362'
     '''
-    import apogee.tools.read as apread
-    from apogee.tools import bitmask
-    from apogee.spec import continuum
-    import astropy
 
-    if allvisit_cat is None:
-        allvisit_cat = apread.allVisit(rmcommissioning = False, ak = False)
+    filepath = os.path.join(master_path, catalog_path, all_visit_name)  # dr14
+    filename = os.path.join(download_path, all_visit_name)
+    if not os.path.exists(filename):
+        print("Downloading : " + catalog_name)
+        subprocess.check_call(["wget", filepath, "-O", "%s"%filename])
+    allvisit_cat = pyfits.getdata(filename)
     where_visits = np.where(allvisit_cat['APOGEE_ID'] == apogee_id)[0]
     
     plate_ids = np.array([int(i) for i in allvisit_cat[where_visits]['PLATE']])
@@ -221,19 +160,29 @@ def get_visit_spectra_individual_object(apogee_id, allvisit_cat = None, save_loc
     vhelios_synth = allvisit_cat[where_visits]['SYNTHVHELIO']
     snrs = allvisit_cat[where_visits]['SNR']
     BCs = allvisit_cat[where_visits]['BC']
-    
-    badcombpixmask = bitmask.badpixmask() + 2**bitmask.apogee_pixmask_int("SIG_SKYLINE")
+
     all_spec, all_err, all_snr, all_hjd, all_vhelio = [], [], [], [], []
     
     for i, pid in enumerate(plate_ids):
+        filepath = os.path.join(apVisit_path, '%s'%pid, '%s'%mjds[i])
+        filename = 'apVisit-r8-%s-%s-%s.fits' % (pid, mjds[i], fiberids[i])
+        filepath = os.path.join(filepath, filename)
+        filename = os.path.join(download_path, filename)
         try:
-            spec = apread.apVisit(pid, mjds[i], fiberids[i], ext=1, header=False)
-            specerr = apread.apVisit(pid, mjds[i], fiberids[i], ext=2, header=False)
-            wave = apread.apVisit(pid, mjds[i], fiberids[i], ext=4, header=False)
-            mask = apread.apVisit(pid, mjds[i], fiberids[i], ext=3, header=False)
-            masterheader = apread.apVisit(pid, mjds[i], fiberids[i], ext=0, header=True)
-            
-            badpix = (mask & (badcombpixmask)) != 0
+            if not os.path.exists(filename):
+                subprocess.check_call(["wget", filepath, '-O', '%s'%filename])
+
+            # read spectrum
+            spec = np.flipud(pyfits.getdata(filename, ext = 1, header = False).flatten())
+            specerr = np.flipud(pyfits.getdata(filename, ext = 2, header = False).flatten())
+            mask = np.flipud(pyfits.getdata(filename, ext = 3, header = False).flatten())
+            wave = np.flipud(pyfits.getdata(filename, ext = 4, header = False).flatten())
+
+            hdulist = pyfits.open(filename)
+            masterheader = hdulist[0].header 
+            hdulist.close()
+    
+            badpix = mask != 0
             if np.sum(badpix)/len(badpix) > 0.5:
                 print('too many bad pixels!')
                 continue # if 50% or more of the pixels are bad, don't bother.
@@ -250,7 +199,7 @@ def get_visit_spectra_individual_object(apogee_id, allvisit_cat = None, save_loc
                 wave = np.copy(standard_grid)
             
             # preliminary normalization using Bovy's visit normalization routine. 
-            cont = continuum.fitApvisit(spec, specerr, wave)
+            cont = fitApvisit(spec, specerr, wave)
             specnorm, errnorm = spec/cont, specerr/cont
         
             # correct for Earth's orbital motion. 
@@ -282,7 +231,7 @@ def get_visit_spectra_individual_object(apogee_id, allvisit_cat = None, save_loc
     
             all_snr.append(snrs[i])
             all_vhelio.append(vhel)
-        except astropy.io.fits.verify.VerifyError:
+        except pyfits.verify.VerifyError:
             print('there was a verification error')
             continue
     all_spec, all_err, all_snr, all_hjd, all_vhelio = np.array(all_spec), \
@@ -343,3 +292,80 @@ def download_visit_spectra_single_object_and_renormalize(apogee_id, p0_single_co
         renorm_specs.append(renorm_spec)
     renorm_specs = np.array(renorm_specs)
     return renorm_specs, all_err, all_snr, all_hjd, all_vhelio 
+
+
+def toAspcapGrid(spec):
+    """
+    Convert a spectrum from apStar grid to the ASPCAP grid (w/o the detector gaps)
+    Adapted from Jo Bovy's APOGEE package
+    """
+    
+    apStarBlu_lo,apStarBlu_hi,apStarGre_lo,apStarGre_hi,apStarRed_lo,apStarRed_hi \
+        = 322, 3242, 3648, 6048, 6412, 8306 # dr12
+    aspcapBlu_start = 0
+    aspcapGre_start = apStarBlu_hi-apStarBlu_lo+aspcapBlu_start
+    aspcapRed_start = apStarGre_hi-apStarGre_lo+aspcapGre_start
+    aspcapTotal = apStarRed_hi-apStarRed_lo+aspcapRed_start
+
+    out= np.zeros(aspcapTotal,dtype=spec.dtype)
+    
+    out[:aspcapGre_start]= spec[apStarBlu_lo:apStarBlu_hi]
+    out[aspcapGre_start:aspcapRed_start]= spec[apStarGre_lo:apStarGre_hi]
+    out[aspcapRed_start:]= spec[apStarRed_lo:apStarRed_hi]
+
+    return out
+
+def fitApvisit(spec, specerr, wave, deg=4, niter=10, usigma=3., lsigma=0.1, cont_pixels=None):
+    """
+    Adapted from Bovy's APOGEE routine
+
+    Continuum fitting routine for apVisit spectra (one spectrum at a time; aspcap method only)
+    INPUT:
+       spec - single spectrum to fit
+       specerr - error on the spectrum; assume no covariances
+       wave - wavelength grid corresponding to spec and specerr; must have length 12288
+       ASPCAP keywords:
+          deg = (4) degree of the polynomial
+          niter = (10) number of sigma-clipping iterations to perform
+          usigma, lsigma = (3., 0.1) upper and lower sigmas for sigma clipping
+    OUTPUT:
+       continuum
+    """
+    # Parse the input
+    tspec = copy.copy(spec)
+    tspecerr = specerr
+    if len(wave) != 12288:
+        raise ValueError('Length of apVisit wavelength array is not 12288; cannot proceed.')
+    if wave[1] < wave[0]: # not sorted by increasing wavelength; fix it
+        wave = np.flipud(wave)
+        tspec = np.flipud(tspec)
+        tspecerr = np.flipud(tspecerr)
+    cont = np.empty_like(tspec)
+    bluewav = wave[0:4096]
+    greenwav = wave[4096:8192]
+    redwav = wave[8192::]
+    # Blue
+    cont[0:4096] = _fit_aspcap(bluewav, tspec[0:4096], tspecerr[0:4096], deg, niter, usigma, lsigma)
+    # Green
+    cont[4096:8192] = _fit_aspcap(greenwav, tspec[4096:8192], tspecerr[4096:8192], deg, niter, usigma, lsigma)
+    # Red
+    cont[8192::] = _fit_aspcap(redwav, tspec[8192::], tspecerr[8192::], deg, niter, usigma, lsigma)
+    return cont
+
+def _fit_aspcap(wav,spec,specerr,deg,niter,usigma,lsigma):
+    """Fit the continuum with an iterative upper/lower rejection"""
+    # Initial fit
+    chpoly= np.polynomial.Chebyshev.fit(wav,spec,deg,w=1./specerr)
+    tcont= chpoly(wav)
+    tres= spec-tcont
+    sig= np.std(tres)
+    mask= (tres < usigma*sig)*(tres > -lsigma*sig)
+    spec[True^mask]= chpoly(wav[True^mask])
+    for ii in range(niter):
+        chpoly= np.polynomial.Chebyshev.fit(wav, spec, deg, w=1./specerr)
+        tcont= chpoly(wav)
+        tres= spec-tcont
+        sig= np.std(tres)
+        mask= (tres < usigma*sig)*(tres > -lsigma*sig)
+        spec[True^mask]= chpoly(wav[True^mask])
+    return chpoly(wav)
